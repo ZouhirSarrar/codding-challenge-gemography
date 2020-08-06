56505d41a8b31bfc3e960281d47e86e88c319b07
Zouhir Sarrar <zouhir.sarrar-ext@cdgcapital.ma>
2020-08-06 20:00:38 +0100

    [feat] Add Chakra UI theme & darkmode button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {ThemeProvider,theme,ColorModeProvider,CSSReset,Flex,Box,IconButton,useColorMode,Text} from "@chakra-ui/core";
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+
+    <ColorModeProvider>
+    <CSSReset/>
+
+    <Boxes/>
+        <Text fontSize='5xl' textAlign='center'>
+          Codding Challenge Gemography
+        </Text>  
+    </ColorModeProvider>
+    </ThemeProvider>
   );
+}
+
+const Boxes = () => {
+
+return(
+  <Flex>
+  <Box>
+  <ModeSelector/>
+
+        </Box>
+  </Flex>
+)
+}
+
+const ModeSelector = () =>{
+  const {colorMode,toggleColorMode}=useColorMode()
+  return(
+    <Box>
+    <IconButton icon={colorMode === 'light'?'moon' : 'sun'} onClick={toggleColorMode}/>
+    </Box>
+  )
 }
 
 export default App;
