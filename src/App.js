cfbb815d70cc444b4c9d3e96a2370a3f5c9ce148
Zouhir Sarrar <zouhir.sarrar-ext@cdgcapital.ma>
2020-08-07 13:06:32 +0100

    [feat][style] Add axios to fetsh JSON data & return a list of cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from "axios";
 import './App.css';
 import {ThemeProvider,theme,ColorModeProvider,CSSReset,Flex,Box,IconButton,useColorMode,Text} from "@chakra-ui/core";
-function App() {
+import Posts from "./components/Posts"
+const  App = () => {
 
+  const [posts,setPosts] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const [currentPage,setCurrentPage] = useState(1);
+  const [postPerPage,setPostPerPage] = useState(10);
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    setLoading(true);
+    const res = await axios.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+    setPosts(res.data.items);
+    setLoading(false);
+  }
+  fetchPosts();
+
+}, []);
+
+console.log(posts)
   return (
     <ThemeProvider theme={theme}>
-
     <ColorModeProvider>
     <CSSReset/>
-
     <Boxes/>
         <Text fontSize='5xl' textAlign='center'>
           Codding Challenge Gemography
         </Text>  
-    </ColorModeProvider>
+        <Posts posts={posts}  loading={loading}/>
+      </ColorModeProvider>
     </ThemeProvider>
   );
 }
@@ -21,7 +39,7 @@ function App() {
 const Boxes = () => {
 
 return(
-  <Flex>
+  <Flex align="center" justify="center">
   <Box>
   <ModeSelector/>
 
