06a952b690b1d3cadddb162d1af52ea4d0e72a20
Zouhir Sarrar <zouhir.sarrar-ext@cdgcapital.ma>
2020-08-07 19:54:28 +0100

    [feat] Add infinite scrolling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React,{useState,useEffect} from 'react';
+
+  
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import './App.css';
-import {ThemeProvider,theme,ColorModeProvider,CSSReset,Flex,Box,IconButton,useColorMode,Text} from "@chakra-ui/core";
-import Posts from "./components/Posts"
-const  App = () => {
+import { Waypoint } from "react-waypoint";
+import {Box,Image,Badge,Flex,Grid,ThemeProvider,theme,ColorModeProvider,CSSReset,Text,useColorMode,IconButton} from '@chakra-ui/core'
 
-  const [posts,setPosts] = useState([]);
-  const [loading,setLoading] = useState(false);
-  const [currentPage,setCurrentPage] = useState(1);
-  const [postPerPage,setPostPerPage] = useState(10);
+const App = () => {
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+    const [hasNextPage, setHasNextPage] = useState(true);
 
-useEffect(() => {
-  const fetchPosts = async () => {
-    setLoading(true);
-    const res = await axios.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
-    setPosts(res.data.items);
-    setLoading(false);
-  }
-  fetchPosts();
+    useEffect(() => {
+        getData();
+    }, []);
 
-}, []);
+    const getData = () => {
+        if (!hasNextPage) return;
 
-console.log(posts)
-  return (
+        const searchUserURL = `https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`;
+        axios.get(searchUserURL).then(({ data: { items, total_count } }) => {
+            if (items) {
+                if (total_count === users.length + items.length) {
+                    setHasNextPage(false);
+                }
+
+                setUsers(users => [...users, ...items]);
+                setPage(page => page + 1);
+            }
+        });
+    };
+
+    const loadMoreData = () => {
+        if (page > 1) {
+            getData();
+        }
+    };
+
+    return (
+        <>
     <ThemeProvider theme={theme}>
     <ColorModeProvider>
     <CSSReset/>
@@ -30,21 +45,78 @@ console.log(posts)
         <Text fontSize='5xl' textAlign='center'>
           Codding Challenge Gemography
         </Text>  
-        <Posts posts={posts}  loading={loading}/>
+            <div className="row">
+                            <ul className="list-group list-group-flush">
+                                {users.map((user, idx) => (
+                                    <UserCard
+                                        key={idx}
+                                        sno={idx + 1}
+                                        {...user}
+                                    />
+                                ))}
+                            </ul>
+                    {hasNextPage && (
+                        <Waypoint onEnter={loadMoreData}>
+                            <h5 className="text-muted mt-5">
+                                Loading data{" "}
+                            </h5>
+                        </Waypoint>
+                    )}
+            </div>
       </ColorModeProvider>
     </ThemeProvider>
-  );
-}
+
+
+        </>
+    );
+};
+
+const UserCard = ({ name, description, id, owner, open_issues_count,stargazers_count}) => {
+    return (
+    <Flex       align='center'
+      justify='flex-end'
+      direction='row'
+      width='80'
+      height='80'
+      borderRadius='40px'
+      margin='16px'
+      padding='16px' >
+
+          <Box >
+            <Image  src={owner.avatar_url} alt={owner.login} align="center" fallbackSrc="https://via.placeholder.com/150"/>
+          </Box>
+
+          <Box>
+                      <Box
+                      mt="1"
+                      fontWeight="bold"
+                      as="h4"
+                      lineHeight="tight"
+                      isTruncated
+            >
+                      {name}
+                    </Box>
+            <Text textAlign="center">{description}</Text>
+            <Box p="2">
+
+           </Box>
+             </Box>       
+    </Flex>
+        
+    );
+};
 
 const Boxes = () => {
 
 return(
-  <Flex align="center" justify="center">
-  <Box>
+
+  <Flex align="center" justify="center" >
+  <Box >
   <ModeSelector/>
 
         </Box>
   </Flex>
+ 
 )
 }
 
@@ -58,3 +130,4 @@ const ModeSelector = () =>{
 }
 
 export default App;
+
