ca2c22db5b0ba316b3ed483aef770ffd7bf8e577
Zouhir Sarrar <zouhir.sarrar-ext@cdgcapital.ma>
2020-08-11 11:25:14 +0100

    [fix] fix the link of cards, making the title of repository clickable

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -13,49 +13,50 @@ const RepoCard = ({
   html_url,
 }) => {
   return (
-    <a href={html_url}>
-      <Flex
-        align="center"
-        width="800px"
-        borderRadius="40px"
-        margin="16px"
-        padding="16px"
-        borderWidth="1px"
-        rounded="lg"
-        overflow="hidden"
-        ml={250}
-      >
-        <div className="div-style">
-          <img className="img-style" src={owner.avatar_url} alt={owner.login} />
-        </div>
+    <Flex
+      align="center"
+      width="800px"
+      borderRadius="40px"
+      margin="16px"
+      padding="16px"
+      borderWidth="1px"
+      rounded="lg"
+      overflow="hidden"
+      ml={270}
+    >
+      <div className="div-style">
+        <img className="img-style" src={owner.avatar_url} alt={owner.login} />
+      </div>
 
-        <div className="div-style-card">
+      <div className="div-style-card">
+        <a href={html_url}>
           <p className="title"> {name} </p>
-          <p className="text-style">{description}</p>
+        </a>
 
-          <div className="row">
-            <div className="column">
-              <Badge rounded="full" px="2" variantColor="teal">
-                Stars <span className="badge_nb">{stargazers_count}</span>
-              </Badge>
-            </div>
-            <div className="column">
-              <Badge rounded="full" px="2" variantColor="teal">
-                Issues <span className="badge_nb">{open_issues_count}</span>
-              </Badge>
-            </div>
-            <div className="column">
-              <Badge rounded="full" px="2" variant="subtle">
-                <span className="badge_nb">Submitted </span>
-                {TimeInterval(`${created_at}`)}
-                <span className="badge_nb"> days ago by </span>
-                {owner.login}{" "}
-              </Badge>
-            </div>
+        <p className="text-style">{description}</p>
+
+        <div className="row">
+          <div className="column">
+            <Badge rounded="full" px="2" variantColor="teal">
+              Stars <span className="badge_nb">{stargazers_count}</span>
+            </Badge>
+          </div>
+          <div className="column">
+            <Badge rounded="full" px="2" variantColor="teal">
+              Issues <span className="badge_nb">{open_issues_count}</span>
+            </Badge>
+          </div>
+          <div className="column">
+            <Badge rounded="full" px="2" variant="subtle">
+              <span className="badge_nb">Submitted </span>
+              {TimeInterval(`${created_at}`)}
+              <span className="badge_nb"> days ago by </span>
+              {owner.login}{" "}
+            </Badge>
           </div>
         </div>
-      </Flex>
-    </a>
+      </div>
+    </Flex>
   );
 };
 export default RepoCard;
